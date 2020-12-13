4e61018b0ce4c19f0c633aa5d00d321320ca3ee4
hai271120 <69028228+hai271120@users.noreply.github.com>
2020-12-13 20:17:58 +0700

    Add files via upload

---
+++ screen/story.js
@@ -0,0 +1,13 @@
+class StoryScreen extends HTMLElement {
+    constructor() {
+        super()
+        this._shadow = this.attachShadow({ mode: 'open' })
+    }
+    connectedCallback() {
+        this._shadow.innerHTML = `
+        <story-header> </story-header>
+        <create-post></create-post>
+        `   
+}
+}
+window.customElements.define('story-screen', StoryScreen)
